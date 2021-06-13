0397625d6339a92758bdf5ff127be148105581ef
jakamm <jakamm@student.fullsail.edu>
2021-06-13 16:27:12 -0400

    Adding Project (01)

---
+++ HideOrDie/Assets/CutsceneTimer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneTimer : MonoBehaviour
+{
+
+    [Header("Clip Length")]
+    public int clipLength;
+
+
+    [Header("Cutscene Timer")]
+    public int cutsceneTimer = 0;
+
+
+    [Header("Next Scene")]
+    public string NextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+
+
+
+        if (cutsceneTimer >= clipLength)
+        {
+
+
+            //Load next level
+            SceneManager.LoadScene(NextScene);
+
+            //Debug
+            Debug.Log("Cutscene over.");
+
+
+        }
+
+        else
+        {
+
+            //increase timer
+            cutsceneTimer += 1;
+
+        }
+
+
+
+
+    }
+}
