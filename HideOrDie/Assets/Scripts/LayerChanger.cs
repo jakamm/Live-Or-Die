c8e9526ad091f098f4ee4e168a94cd642ce3281b
CaptnDino <workwithcaptndino@gmail.com>
2021-08-18 02:10:11 -0400

    Creating scripts, animations for doors and traps

---
+++ HideOrDie/Assets/Scripts/LayerChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerChanger : MonoBehaviour
+{
+    public int newLayer;
+    private int defaultLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultLayer = this.gameObject.layer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Contains("Player"))
+        {
+            this.gameObject.layer = newLayer;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Contains("Player"))
+        {
+            this.gameObject.layer =  defaultLayer;
+        }
+    }
+}
---
+++ HideOrDie/Assets/Scripts/TrapWire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapWire : MonoBehaviour
+{
+
+    public int damageAmount;
+    public bool canDestroy = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Contains("Player"))
+        {
+            other.gameObject.GetComponent<HealthManager>().ApplyDamage(damageAmount);
+            if (canDestroy)
+                Destroy(this.gameObject);
+        }
+    }
+
+}
