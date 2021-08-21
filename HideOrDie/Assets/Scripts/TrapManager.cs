8ed7eb6e30d21c8d9de44fb86ce1524caf1e98a1
CaptnDino <workwithcaptndino@gmail.com>
2021-08-21 02:15:35 -0400

    Created script for handling traps and finished level design

---
+++ HideOrDie/Assets/Scripts/TrapManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapManager : MonoBehaviour
+{
+
+    public Animation[] animations;
+
+    public void DisableTrap()
+    {
+        for(int i = 0; i < animations.Length; i++)
+        {
+            animations[i].Stop();
+        }
+        Debug.Log("off");
+
+    }
+
+    public void EnableTrap()
+    {
+
+        for (int i = 0; i < animations.Length; i++)
+        {
+            animations[i].Play();
+        }
+        Debug.Log("on");
+    }
+}
