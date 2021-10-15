7b72d91641b87a13613b25fb86446ac5ab917dbc
jakamm <jakamm@student.fullsail.edu>
2021-10-15 18:08:43 -0400

    Importing Andy's Extras Menu
    Everything that is needed for Andy's extras menu to work has been added.

---
+++ HideOrDie/Assets/AAAndy/CollectibleObjectList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "CollectibleList")]
+public class CollectibleObjectList : ScriptableObject
+{
+    public List<VideoClip> ClipList;
+    public List<VideoClip> LetterList;
+    [TextArea]
+    public List<string> LetterContentList;
+}
